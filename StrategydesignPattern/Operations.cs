586c6b2b3a5eac6943f295002703fc4f8f051312
Joe Trobisch <jtrobisch@walsallcollege.ac.uk>
2019-10-31 13:07:16 +0000

    Add project files.

---
+++ StrategydesignPattern/Operations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategydesignPattern
+{
+    public class OperationAdd: Strategy
+    {
+        public int doOperation(int num1, int num2) {
+            return num1 + num2;
+        }
+    }
+
+    public class OperationSubtract : Strategy
+    {
+        public int doOperation(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+
+    public class OperationMultiply : Strategy
+    {
+        public int doOperation(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
---
+++ StrategydesignPattern/Strategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategydesignPattern
+{
+    public interface Strategy
+    {
+        int doOperation(int num1, int num2);
+    }
+}
